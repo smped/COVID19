c6106abd6d21bd3aaba90a3ca8c47862b1f199ac
Steve Ped <stephen.pederson@adelaide.edu.au>
2020-12-20 22:33:41 +1030

    Rewrote entire AU sheet

--- US_States.R
+++ US_States.R
@@ -24,6 +24,28 @@ nyt <- "https://raw.githubusercontent.com/alex/nyt-2020-election-scraper/master/
     ) %>%
     mutate(Party = str_to_title(Party))
 
+# Governors
+gov <- "https://en.wikipedia.org/wiki/List_of_current_United_States_governors" %>%
+    read_html() %>%
+    html_nodes("body") %>%
+    xml_find_all("//table[contains(@class, 'sortable wikitable')]") %>%
+    .[[1]] %>%
+    html_table(header = FALSE, fill = TRUE) %>%
+    dplyr::select(c(1, 3, 5)) %>%
+    as_tibble() %>%
+    dplyr::slice(-1, -2) %>%
+    setNames(c("State", "Governor", "Party")) %>%
+    mutate(
+        Party = str_extract(Party, "Republican|Democratic")
+    ) %>%
+    bind_rows(
+        tibble(
+            State = "District of Columbia",
+            Governor = NA_character_,
+            Party = "Democratic"
+        )
+    )
+
 # Population Density
 dens <- "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population_density" %>%
     read_html() %>%
@@ -58,7 +80,8 @@ df %>%
     ) %>% 
     dplyr::select(pop, Death_Rate, everything()) %>%
     arrange(Death_Rate) %>%
-    left_join(nyt, by = c("Province_State" = "State")) %>%
+    # left_join(nyt, by = c("Province_State" = "State")) %>%
+    left_join(gov, by = c("Province_State" = "State")) %>%
     mutate(
         Party = str_extract(Party, "Democrat|Republican"),
         State = fct_inorder(Province_State)
@@ -82,7 +105,8 @@ df %>%
     ) %>% 
     dplyr::select(pop, Death_Rate, everything()) %>%
     arrange(Death_Rate) %>%
-    left_join(nyt, by = c("Province_State" = "State")) %>%
+    # left_join(nyt, by = c("Province_State" = "State")) %>%
+    left_join(gov, by = c("Province_State" = "State")) %>%
     left_join(dens, by = c("Province_State" = "State")) %>%
     mutate(
         Party = str_extract(Party, "Democrat|Republican"),
@@ -99,6 +123,7 @@ df %>%
         x = "Population / km2",
         y = "Deaths / Million"
     )
+
 
 df %>% 
     mutate(
